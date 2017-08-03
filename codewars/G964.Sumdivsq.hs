29cb4c3472ca34b1f6eeff25f609d9db6b065e43
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2017-08-03 23:00:31 +0200

    Integers: Recreation One

---
+++ codewars/G964.Sumdivsq.hs
@@ -0,0 +1,15 @@
+-- https://www.codewars.com/kata/integers-recreation-one/train/haskell
+module Codewars.G964.Sumdivsq where
+
+listSquared :: Int -> Int -> [(Int, Int)]
+listSquared m n = [m..n] >>= (\x -> [x, (divisors x n)]) >>= 
+
+divisors acc n = 
+  let 
+    (d, r) = n `div` acc
+  in
+    if acc * acc > n then [n]
+    else if r /= 0 then divisors (acc + 1) n
+    else if d < acc then d : acc : divisors (acc + 1) n
+    else d : divisors (acc + 1) n
+    
