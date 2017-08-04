f548301b7fd17158248bfffb95c12ab276c712e6
Functional Programming <fp@Evgenys-MacBook-Air.local>
2017-08-05 00:17:05 +0200

    implementation for LandPerimeter

--- codewars/LandPerimeter.hs
+++ codewars/LandPerimeter.hs
@@ -1,5 +1,22 @@
 -- https://www.codewars.com/trainer/haskell
 module Kata (landPerimeter) where
+import Data.List (transpose)
 
 landPerimeter :: [String] -> String
-landPerimeter arr = error "Implement me!
+landPerimeter arr = "Total land perimeter: " ++ show val
+    where
+        val = 4 * units arr - 
+              2 * neibors arr - 
+              2 * neibors tra
+        tra = transpose arr
+
+units :: [String] -> Int
+units arr = sum . map count1 $ arr
+    where count1 = length . filter (== 'X')
+
+neibors :: [String] -> Int
+neibors arr= sum . map countN $ arr
+    where
+        countN ln@('X':'X':_) = 1 + (countN . tail $ ln)
+        countN [] = 0
+        countN (_:xs) = 0 + countN xs
