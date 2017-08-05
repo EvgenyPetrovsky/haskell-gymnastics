edea71d3d8ed950ef008aba2eff73e22d6b4e94b
Functional Programming <ev.petrovsky@gmail.com>
2017-08-05 22:49:24 +0200

    aplmost done but still not ready

--- codewars/G964.Sumdivsq.hs
+++ codewars/G964.Sumdivsq.hs
@@ -2,13 +2,12 @@
 module Codewars.G964.Sumdivsq where
 
 listSquared :: Int -> Int -> [(Int, Int)]
-listSquared m n = [m..n] >>= (\x -> [x, (divisors x n)]) >>= 
+listSquared m n = filter (\ x -> testSqrt (snd x)) $ map pair [m..n]
+  --filter (\x -> testsqrt . snd $ x) $ map (sum . pair) [m..n]
+  where 
+    pair x = (x, sum . divisors $ x)
+    testSqrt x = (intSqrt x) * (intSqrt x) == x
+    intSqrt x = truncate . sqrt $ x
 
-divisors acc n = 
-  let 
-    (d, r) = n `div` acc
-  in
-    if if r /= 0 then divisors (acc + 1) n
-    else if d < acc then d : acc : divisors (acc + 1) n
-    else d : [n]
-    
+divisors :: Int -> [Int]
+divisors n = filter (\x -> n `mod` x == 0) [1..n]
