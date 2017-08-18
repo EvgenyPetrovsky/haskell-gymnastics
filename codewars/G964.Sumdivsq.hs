fd048a4f094ff69f1b6d688f52a2e6163e11fa5e
Functional Programming <ev.petrovsky@gmail.com>
2017-08-18 07:22:32 +0200

    working version but still fails tests

--- codewars/G964.Sumdivsq.hs
+++ codewars/G964.Sumdivsq.hs
@@ -2,12 +2,15 @@
 module Codewars.G964.Sumdivsq where
 
 listSquared :: Int -> Int -> [(Int, Int)]
-listSquared m n = filter (\ x -> testSqrt (snd x)) $ map pair [m..n]
-  --filter (\x -> testsqrt . snd $ x) $ map (sum . pair) [m..n]
+listSquared m n = filter ( testSqrt . snd ) $ map pair [m..n]
   where 
     pair x = (x, sum . divisors $ x)
-    testSqrt x = (intSqrt x) * (intSqrt x) == x
-    intSqrt x = truncate . sqrt $ x
 
 divisors :: Int -> [Int]
 divisors n = filter (\x -> n `mod` x == 0) [1..n]
+
+testSqrt :: Int -> Bool
+testSqrt x = (intSqrt x * intSqrt x) == x
+
+intSqrt :: Int -> Int
+intSqrt x = truncate . sqrt . fromIntegral $ x
