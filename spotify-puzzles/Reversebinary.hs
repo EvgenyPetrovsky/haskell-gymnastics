812a8353863a595704ac64939a312e988eee9365
Evgeny Petrovsky <EvgenyPetrovsky@users.noreply.github.com>
2017-09-04 15:52:00 +0200

    finished program

--- spotify-puzzles/Reversebinary.hs
+++ spotify-puzzles/Reversebinary.hs
@@ -1,3 +1,16 @@
 -- ProblemID: Reversebinary
 
-reverseBinary :: -> Int -> Int
+reverseBinary :: Integral n => n -> n
+reverseBinary = (unDigits 2) . reverse . (digits 2)
+
+digits :: Integral n => n -> n -> [n]
+digits base num = reverse $ digitsR base num
+  where
+    digitsR base num
+    | num == 0 = []
+    | otherwise = r : (digitsR base v)
+        where (v,r) = divMod num base
+
+unDigits :: Integral n => n -> [n] -> n
+unDigits base num = sum . zipWith (*) baseExp $ reverse num
+  where baseExp = [ base^x | x <- [0..]]
