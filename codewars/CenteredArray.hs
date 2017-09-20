e5eb8ef1017404a9b64e764bc09ea1ae559d2d1b
Functional Programming <ev.petrovsky@gmail.com>
2017-09-20 23:44:49 +0200

    version based on scanl

--- codewars/CenteredArray.hs
+++ codewars/CenteredArray.hs
@@ -2,20 +2,10 @@
 module Codewars.CenteredArray where
   
 isCentered :: [Int] -> Int -> Bool
-isCentered xs n
-  | length xs == 0 = False
-  | n == 0 && length xs `rem` 2 == 0 = True
-  | otherwise = isCentered' t (n-h)
-  where (h:t) = foldInMid xs
+isCentered xs n = elem n $ scanl (+) 0 (foldInMid xs)
 
-isCentered' :: [Int] -> Int -> Bool
-isCentered' _ 0 = True
-isCentered' [] _ = False
-isCentered' (x:xs) n = isCentered' xs (n-x)
-
-foldInMid xs = 
-  | m == 0 = zipWith (+) (reverse l) r
-  | otherwise = (head r) : zipWith (+) (reverse l) (tail r)
-  where 
-    (d,m) = length xs `divMod` 2
+foldInMid :: [Int] -> [Int]
+foldInMid xs = reverse $ zipWith (+) (reverse r) (l++[0])
+  where
+    d = length xs `div` 2
     (l,r) = splitAt d xs
