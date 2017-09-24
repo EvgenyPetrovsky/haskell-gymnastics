73a6aefa3bd47d0fb752b5305abe4e157818bac8
Evgeny Petrovskiy <ev.petrovsky@gmail.com>
2017-09-24 21:35:26 +0200

    triangles b o zipWith

--- codewars/Triangles.hs
+++ codewars/Triangles.hs
@@ -4,5 +4,4 @@ module Codewars.Triangles where
 import Data.List (sort)
 
 isTriangle :: Int -> Int -> Int -> Bool
-isTriangle a b c = (s1 + s2) > s3
-    where [s1, s2, s3] = sort [a, b, c]
+isTriangle a b c = (<) 0 $ sum . zipWith (*) [1,1,-1] $ sort [a,b,c]
