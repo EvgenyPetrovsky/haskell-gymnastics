78e1df7a6526036336ecf627a02c8340db065e00
Evgeny Petrovskiy <ev.petrovsky@gmail.com>
2017-10-02 22:27:28 +0200

    simpler version
    use comparison of side lengths and "and"

--- codewars/Triangles.hs
+++ codewars/Triangles.hs
@@ -4,4 +4,4 @@ module Codewars.Triangles where
 import Data.List (sort)
 
 isTriangle :: Int -> Int -> Int -> Bool
-isTriangle a b c = (<) 0 $ sum . zipWith (*) [1,1,-1] $ sort [a,b,c]
+isTriangle a b c = and [a + b > c, b + c > a, c + a > b]
