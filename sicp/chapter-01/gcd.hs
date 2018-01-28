992dcf4b4f12aec284d40239a5d84854852724a8
Evgeny Petrovskiy <ev.petrovsky@gmail.com>
2018-01-28 23:09:24 +0300

    fix errors of draft version

--- sicp/chapter-01/gcd.hs
+++ sicp/chapter-01/gcd.hs
@@ -1,6 +1,6 @@
-gcd :: Int -> Int -> Int
+gcd' :: Int -> Int -> Int
 
-gcd a b = 
-    | rem_ab == 0 = b
-    | otherwise = gcd b rem_ab
-    where rem_ab = rem a b
+gcd' a b
+  | rem_ab == 0 = b
+  | otherwise   = gcd' b rem_ab
+  where rem_ab = rem a b
