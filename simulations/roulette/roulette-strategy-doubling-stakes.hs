0ede0d5452308e3ac291adb18e4d4d4e6bed72df
Evgeny Petrovskiy <ev.petrovsky@gmail.com>
2018-03-07 23:57:59 +0100

    doubling stakes strategy

--- simulations/roulette/roulette-strategy-doubling-stakes.hs
+++ simulations/roulette/roulette-strategy-doubling-stakes.hs
@@ -12,57 +12,26 @@
 --}
 
 import qualified Roulette.Definitions as RD
-
-
-gamesCount = 100
-
-{--
-  Generate players for game.
-  Input is required number of players to be generated; output is list of Players
---}
-generatePlayers :: Int -> [Player]
-generatePlayers n b = 
-  map (createPlayer balance) [1..n]
-  where
-    balance = b
-    createPlayer bal luckyNum = 
-      initPlayer bal (makeGenerator luckyNum)
-
-
-{--
-  Run simulation requires player, house and roulette, 
-  luck of player (R.StdGen) defines 
---}
-runManySimulations :: Int -> [Player]
-runManySimulations n =
-  map (\p -> runSimulation p house) $ generatePlayers n
-  where
-    house = RD.defaultHouse
-
-{--
-  Complete lifecycle of player in casino
-  Fold. Zero element (house, player), over winning numbers, returns Player
---}
-runSimulation :: Player -> House -> Player
-runSimulation p h =
-  fold? pockets p
-  where 
-    luck = luck p
-    style = wheelStype h
-    pockets = take gamesCount (winningNumbers style luck)
+import qualified Roulette.Simulations as RS
 
 {--
   implementation of one game round, always use one placement - Red
   if no experience - make minimun bet
   if some experience and last game was won - make 
 --} 
-strategyRed :: House -> Player -> Pocket -> Player
-iter pcts h p = 
-  if experience p == [] then
-
+strategyRed :: Player -> Game -> Player
+strategyRed p g = 
+  if noExperience p then 
+    addBet (RedBet, minBet) initBets
+  else if lastPayout == 0 && dblBet <= maxBet
+    addBet (RedBet, dblBet) initBets
   else 
-
--- check balance; play round; extend experience
-playTheGame
-
-data Player = 
+    addBet (RedBet, minBet) initBets
+  where 
+    noExperience p = experience p == []
+    minBet = minBet g
+    maxBet = maxBet g
+    lastBet = head . experience $ p
+    lastPayout = payout lastBet
+    lastBetCost = betsCost lastBet
+    dblBet = lastBetCost * 2
